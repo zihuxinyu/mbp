f64583e2859c5d9db011ed13be32fc52a3494e44
weibaohui <zihuxinyu@163.com>
2014-03-15 16:22:33 +0800

    增加nginx反向代理

---
+++ clean.sh
@@ -0,0 +1,1 @@
+find mbp -name '*.pyc' -exec rm -f {} ';'
--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 #!env/bin/python
 # -*- coding: utf8 -*-
+import sys
 from mbp import app
 
-app.run(debug=True, host='0.0.0.0',port=5000)
+if len(sys.argv)>1:
+    port = int(sys.argv[1])
+else:
+    port=5000
+app.run(debug=True, host='0.0.0.0',port=port)
 
