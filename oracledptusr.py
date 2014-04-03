6b55b838c5090e6ec3c43e796d1cfcaa4a5fed16
weibaohui <zihuxinyu@163.com>
2014-04-03 09:35:42 +0800

    增加自动执行sql功能

--- oracledptusr.py
+++ oracledptusr.py
@@ -10,29 +10,24 @@ def db():
                                  user='weibh',
                                  password='1234')
 
-
-
-
-
-
-
 if __name__ == "__main__":
 
 
     xx=db().get("select * from oraclerun")
     print(xx.CN)
 
-    man_file = open('man_data.txt', 'w')
+    man_file = open('man_data.txt', 'w',encoding='utf-8')
     lines=" ({0}, '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', NULL, NULL), \r\n"
     man_file.writelines("INSERT INTO `portal_user` (`guid`, `user_code`, `user_name`, `user_mobile`, `dpt_name`, "
                         "`topdpt`, `manager`, `msg`, `msgexpdate`) VALUES")
     list = db().query("select * from EXT_DPT_USR t")
     i=0
     for x in list:
-        #print(x.user_name, x.user_code)
+
+        #print(x.user_code, x.user_name, x.user_mobile, x.dpt_name, x.topdpt, x.manager)
         man_file.writelines(lines.format(i,x.user_code,x.user_name,x.user_mobile,x.dpt_name,x.topdpt,x.manager))
         i=i+1
-    
+
     man_file.close()
 
 
