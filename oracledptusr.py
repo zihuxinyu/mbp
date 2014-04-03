e8129a99c1a2d96118289a714d8471b5326d174d
weibaohui <zihuxinyu@163.com>
2014-04-03 09:48:48 +0800

    增加自动执行sql功能

--- oracledptusr.py
+++ oracledptusr.py
@@ -18,13 +18,15 @@ if __name__ == "__main__":
 
     man_file = open('man_data.txt', 'w',encoding='utf-8')
     lines=" ({0}, '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', NULL, NULL), \r\n"
-    man_file.writelines("INSERT INTO `portal_user` (`guid`, `user_code`, `user_name`, `user_mobile`, `dpt_name`, "
+    man_file.writelines("TRUNCATE portal_user; INSERT INTO `portal_user` (`guid`, `user_code`, `user_name`, `user_mobile`, `dpt_name`, "
                         "`topdpt`, `manager`, `msg`, `msgexpdate`) VALUES")
     list = db().query("select * from EXT_DPT_USR t")
-    i=0
+    i=1
     for x in list:
+        #去除最后的逗号
+        l=len(list)
+        lines=lines.rstrip(', \r\n') if (i==l) else lines
 
-        #print(x.user_code, x.user_name, x.user_mobile, x.dpt_name, x.topdpt, x.manager)
         man_file.writelines(lines.format(i,x.user_code,x.user_name,x.user_mobile,x.dpt_name,x.topdpt,x.manager))
         i=i+1
 
