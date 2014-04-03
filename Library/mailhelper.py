aef4d6220330d7524102193d1f2e8a070a6bfddc
weibaohui <zihuxinyu@163.com>
2014-04-03 17:10:00 +0800

    增加自动执行sql功能

---
+++ Library/mailhelper.py
@@ -0,0 +1,111 @@
+
+# coding: utf-8
+import os
+import mimetypes
+from email.encoders import encode_base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from Library.threadinghelper import asyncfun
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+
+@asyncfun
+def sendMail(subject, text, *attachmentFilePaths):
+    mailUser = 'sd-lcgly@chinaunicom.cn'
+    mailPassword = 'wbh123!!'
+    recipient = 'sd-lcgly@chinaunicom.cn'
+
+    msg = MIMEMultipart()
+    msg['From'] = mailUser
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text))
+
+    for attachmentFilePath in attachmentFilePaths:
+        msg.attach(getAttachment(attachmentFilePath))
+
+    mailServer = smtplib.SMTP('sd.smtp.chinaunicom.cn')
+    mailServer.ehlo()
+    mailServer.ehlo()
+    mailServer.login(mailUser, mailPassword)
+    mailServer.sendmail(mailUser, recipient, msg.as_string())
+    mailServer.close()
+
+    print('Sent email to %s' % recipient)
+
+
+def getAttachment(attachmentFilePath):
+    contentType, encoding = mimetypes.guess_type(attachmentFilePath)
+
+    if contentType is None or encoding is not None:
+        contentType = 'application/octet-stream'
+
+    mainType, subType = contentType.split('/', 1)
+    file = open(attachmentFilePath, 'rb')
+
+    if mainType == 'text':
+        attachment = MIMEText(file.read())
+    elif mainType == 'message':
+        attachment = smtplib.email.message_from_file(file)
+    elif mainType == 'image':
+        attachment = MIMEImage(file.read(), _subType=subType)
+    elif mainType == 'audio':
+        attachment = MIMEAudio(file.read(), _subType=subType)
+    else:
+        attachment = MIMEBase(mainType, subType)
+    attachment.set_payload(file.read())
+    encode_base64(attachment)
+
+    file.close()
+
+    attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(attachmentFilePath))
+    return attachment
+
+
+
+
+
+def getAttach(prefx=None,path='tmp/'):
+    """
+    收取特定标识前缀的附件
+    :param prefx:
+    """
+    import poplib
+    from email import parser
+    
+    host = 'sd.pop.chinaunicom.cn'
+    username = 'sd-lcgly'
+    password = 'wbh123!!'
+
+    pop_conn = poplib.POP3(host)
+    pop_conn.user(username)
+    pop_conn.pass_(password)
+    num, total_size = pop_conn.stat()
+    for i in range(num):
+        emsg = pop_conn.retr(i + 1)[1]
+        message=parser.Parser().parsestr("\n".join(emsg))
+        print(message["Subject"], message["From"], message["To"]        )
+        for part in message.walk():
+
+            fileName = part.get_filename()
+            contentType = part.get_content_type()
+            #print(contentType)
+            # 保存附件
+            if fileName:
+                data = part.get_payload(decode=True)
+
+                fEx = open(fileName, 'wb')
+                fEx.write(data)
+                fEx.close()
+                print(fileName,'ok')
+            #elif contentType == 'text/plain' or contentType == 'text/html':
+            #    #保存正文
+            #    data = part.get_payload(decode=True)
+            #    print(data)
+
+        pop_conn.dele(i + 1)
+
+    pop_conn.quit()
--- oracledptusr.py
+++ oracledptusr.py
@@ -6,16 +6,16 @@
 from Library.ziphelper import getzipfile
 
 from Library.DB import tornoracle3
-
+from Library.config import O_database, O_host, O_password, O_port, O_user
 import os
 
 
 def db():
-    return tornoracle3.Connection(host='134.44.36.51',
-                                  port='1521',
-                                  database='dydb',
-                                  user='weibh',
-                                  password='1234')
+    return tornoracle3.Connection(host=O_host,
+                                  port=O_port,
+                                  database=O_database,
+                                  user=O_user,
+                                  password=O_password)
 
 
 if __name__ == "__main__":
@@ -34,24 +34,28 @@ if __name__ == "__main__":
 
     lines = "({0}, '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', NULL, NULL), \r\n"
 
-    man_file = open(tmpsqlpath, 'w', encoding='utf-8')
-    man_file.writelines(head.format(tablename))
+    #man_file = open(tmpsqlpath, 'w', encoding='utf-8')
+    #man_file.writelines(head.format(tablename))
 
-    list = db().query(selectsql)
-    i = 1
-    for x in list:
-        #去除最后的逗号
-        l = len(list)
-        lines = lines.rstrip(', \r\n') if (i == l) else lines
-        man_file.writelines(lines.format(i, x.user_code, x.user_name, x.user_mobile, x.dpt_name, x.topdpt, x.manager))
-        i += 1
-    
-    man_file.close()
+    # list = db().query(selectsql)
+    # i = 1
+    # for x in list:
+    #     #去除最后的逗号
+    #     l = len(list)
+    #     lines = lines.rstrip(', \r\n') if (i == l) else lines
+    #     man_file.writelines(lines.format(i, x.user_code, x.user_name, x.user_mobile, x.dpt_name, x.topdpt, x.manager))
+    #     i += 1
+    #
+    # man_file.close()
+    #
+    # #压缩文件打包
+    # getzipfile(filepath=tmpsqlpath, zipname=tmpzippath)
+    # #删除文件
+    # os.remove(tmpsqlpath)
 
-    #压缩文件打包
-    getzipfile(filepath=tmpsqlpath, zipname=tmpzippath)
-    #删除文件
-    os.remove(tmpsqlpath)
+    from Library.mailhelper import   sendMail,get1
 
 
-
+    # start to test
+    sendMail(tablename, 'Send a email with Gmail', tmpzippath)
+    get1()
