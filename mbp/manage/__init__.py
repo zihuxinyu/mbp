7cbb47bf5aa2077a6c672148e57b321089e37ace
weibaohui <zihuxinyu@163.com>
2014-04-10 17:12:22 +0800

    增加对flask-admin 中文初始化

--- mbp/manage/__init__.py
+++ mbp/manage/__init__.py
@@ -1,8 +1,6 @@
 # coding: utf-8
+from  flask import  request,session
 from flask.ext.babelex import Babel
-
-__author__ = 'weibaohui'
-
 from mbp import  app,db
 from flask.ext.admin import Admin
 from mbp.manage.views import MyView,MissionBarcode
@@ -11,15 +9,22 @@ from mbp.models import mission as Mmission
 
 babel = Babel(app)
 admin = Admin(app)
-admin.add_view(MyView(name='Hello 1', endpoint='test1', category='Test'))
-admin.add_view(MyView(name='Hello 2', endpoint='test2', category='Test'))
-admin.add_view(MyView(name='Hello 3', endpoint='test3', category='Test'))
+
 admin.add_view(ModelView(Mmission, db.session, category='核查任务', name='任务管理'))
-admin.add_view(MissionBarcode(db.session, category='核查任务', name='资产标签管理',))
+admin.add_view(MissionBarcode(db.session, category='核查任务', name='资产标签管理' ))
 
 
 @babel.localeselector
 def get_locale():
-    # Put your logic here. Application can store locale in
-    # user profile, cookie, session, etc.
-    return 'zh'
+
+    """
+    设置语言
+
+    :return:
+    """
+    override = request.args.get('lang')
+
+    if override:
+        session['lang'] = override
+
+    return session.get('lang', 'zh')
--- mbp/manage/views.py
+++ mbp/manage/views.py
@@ -2,12 +2,15 @@
 from flask.ext.admin import Admin, BaseView, expose
 from flask import g
 from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.admin.form import rules
+from markupsafe import Markup
+from sqlalchemy.event import listens_for
 from wtforms import SelectField
+from mbp.models import mission_barcode
 
 
-
-
-
+def _getMissionHref(view, context, model, name):
+    return Markup("<a href='/index?mid={0}' target=_blank>{1}</a>".format(model.missionid, model.missionid))
 
 
 class MyView(BaseView):
@@ -20,18 +23,48 @@ class MyView(BaseView):
 
 
 class MissionBarcode(ModelView):
+
+    #edit_template = 'admin/edit.html'
     form_overrides = dict(missionid=SelectField)
     form_args = dict(
         # Pass the choices to the `SelectField`
 
         missionid=dict(
-            choices=[(0, 'waiting'), (1, 'in_progress'), (2, 'finished')]
+            choices=[('0', 'waiting'), ('1', 'in_progress'), ('2', 'finished')]
         ))
+
+    form_create_rules = [
+        rules.Header("dsdsdsd"),
+        rules.HTML('''
+        <font color=red>ddd</font>
+        '''),
+
+        rules.FieldSet(('missionid', 'barcode'), '任务信息'),
+
+    ]
+
+
+    column_formatters = {
+        'missionid': _getMissionHref
+    }
+    #可以创建新的
     can_create = True
 
-    # Override displayed fields
+    #显示字段
     column_list = ('missionid', 'barcode')
+    #设置过滤器
+    column_filters = ('missionid', 'barcode')
+    #描述
+    #column_descriptions = {'missionid':'任务ID', 'barcode':'资产标签号'}
+    #字段显示名称
+    column_labels = {'missionid': '任务ID', 'barcode': '资产标签号'}
+    #搜索字段
+    column_searchable_list = [mission_barcode.barcode]
+    #默认排序字段
+    column_default_sort = ('barcode', True)
+    #列表每页数量
+    page_size = 10
     def __init__(self, session, **kwargs):
         # You can pass name and other parameters if you want to
-        from mbp.models import mission_barcode
+
         super(MissionBarcode, self).__init__(mission_barcode, session, **kwargs)
